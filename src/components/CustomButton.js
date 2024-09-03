27e00d200d920eebc0e3ecdc56e730686b8c31cb
Diamond Dai <diamond.dai60@gmail.com>
2024-09-02 18:27:05 -0600

    CustomButtons color them change

--- src/components/CustomButton.js
+++ src/components/CustomButton.js
@@ -1,16 +1,16 @@
 export default function CustomButton({btnLabel, onClickFn, hrefLink}){
     if(hrefLink !== null && hrefLink != ""){
         return (
-            <button className="text-3xl bg-indigo-900 border-indigo-500 border-4 rounded-md p-4 m-4">
+            <button className="text-3xl bg-gray-500 border-gray-300 border-4 rounded-md p-4 m-4">
                 <a href={hrefLink}>
-                    <span className="text-indigo-200">{btnLabel}</span>
+                    <span className="text-gray-200">{btnLabel}</span>
                 </a>
             </button>
         )
     }
     return (
-        <button className="text-3xl bg-indigo-900 border-indigo-500 border-4 rounded-md p-4 m-4" onClick={onClickFn}>
-            <span className="text-indigo-200">{btnLabel}</span>
+        <button className="text-3xl bg-gray-500 border-gray-300 border-4 rounded-md p-4 m-4" onClick={onClickFn}>
+            <span className="text-gray-200">{btnLabel}</span>
         </button>
     );
 }
