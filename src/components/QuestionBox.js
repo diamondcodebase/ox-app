161812627c57dbbf4fbaba7a297a5cd591ea7e8a
Diamond Dai <diamond.dai60@gmail.com>
2024-09-02 23:12:03 -0600

    Add a border of QuestionBox

--- src/components/QuestionBox.js
+++ src/components/QuestionBox.js
@@ -8,7 +8,7 @@ export default function QuestionBox({question, visible, chosenPhrase, answered, 
     ) : (<></>);
     
     const boxContent = visible ? (
-        <div className='questionBox bg-yellow-100 border-indigo-900 border-20 rounded-md p-3 flex flex-col items-center'>
+        <div className='questionBox bg-yellow-100 border-indigo-900 border-2 rounded-md p-3 flex flex-col items-center'>
             <h3 className='row-auto text-4xl text-green-900 mb-4'> {question} </h3>
             {chosenPart}
         </div>) : (<></>);
