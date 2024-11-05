1a06dae6171f740c8731e916cc3501dfeee7538e
diamond_dai <diamond.dai@gmail.com>
2024-11-04 22:45:49 -0700

    insert a testing paragraph

--- src/app/about/page.js
+++ src/app/about/page.js
@@ -14,7 +14,7 @@ export default function About() {
             <main className="flex min-h-screen flex-col items-center justify-between p-24">
                 <div className='text-3xl font-bold'>About</div>
                 <div className="z-10 max-w-5xl w-full items-center justify-between font-mono text-sm lg:flex">
-                    
+                    <p>This is a testing paragraph</p>
                 </div>
                 <BasicButtonPanel />
             </main>
