cf395a527eca970ff879929bcaba2af39a8f32f8
diamond_dai <diamond.dai@gmail.com>
2024-11-08 22:01:46 -0700

    modify AnswerBox

--- src/components/AnswerBox.js
+++ src/components/AnswerBox.js
@@ -2,12 +2,12 @@ export default function AnswernBox({isAnswered, userAnswer, modelAnswer, explana
     const isCorrect = userAnswer === modelAnswer;
     const responsePart = 
         (<div className='row-auto text-4xl text-green-900 justify-center'>
-            <span> {isAnswered ? (isCorrect ? ("Owesome！") : ("Oop...")) : ("Time's up!")}</span>
+            <span> {isAnswered ? (isCorrect ? ("Owesome！") : ("Oop... your guess is wrong.")) : ("Time's up!")}</span>
         </div>
         );
 
     const boxContent = visible ? (
-        <div className='flex flex-col items-center justify-between bg-yellow-100 border-indigo-900 border-10 rounded-sm p-3'>
+        <div className='flex flex-col items-center justify-between bg-yellow-300/80 border-indigo-900 border-2 rounded-md p-3'>
             <div className='text-3xl text-green-900'> {responsePart} </div>
             <div className='text-3xl text-green-900'> {explanation} </div>
         </div>) : (<></>);
