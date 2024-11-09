fd40097b48ceab92fc6a2444dcda87aa7fc147c6
diamond_dai <diamond.dai@gmail.com>
2024-11-08 22:01:59 -0700

    modify QuestionBox

--- src/components/QuestionBox.js
+++ src/components/QuestionBox.js
@@ -1,15 +1,15 @@
 export default function QuestionBox({question, visible, chosenPhrase, answered, showAnswer }) {
 
     const chosenPart = answered ? (
-        <div className='row-auto text-4xl text-green-900 justify-center'>
+        <div className='row-auto text-4xl bg-white/70 border-indigo-900 border-2 rounded-md p-3 text-gray-700 justify-center'>
             {chosenPhrase}
             <span>{showAnswer}</span>
         </div>
     ) : (<></>);
     
     const boxContent = visible ? (
-        <div className='questionBox bg-yellow-100 border-indigo-900 border-2 rounded-md p-3 flex flex-col items-center'>
-            <h3 className='row-auto text-4xl text-green-900 mb-4'> {question} </h3>
+        <div className='questionBox bg-white/70 border-indigo-900 border-2 rounded-md p-3 flex flex-col items-center'>
+            <h3 className='row-auto text-4xl text-indigo-900 mb-4'> {question} </h3>
             {chosenPart}
         </div>) : (<></>);
 
