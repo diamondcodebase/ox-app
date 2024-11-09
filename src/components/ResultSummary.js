498cdc047b18a4caeaead2a2f8e296688f812592
diamond_dai <diamond.dai@gmail.com>
2024-11-08 22:02:23 -0700

    change ResultSummary language to english

--- src/components/ResultSummary.js
+++ src/components/ResultSummary.js
@@ -1,19 +1,19 @@
 export default function ResultSummary({ score, round, visible }) {
-    let endStatement = "謝謝你的參與";
-    let endStatement2 = "繼續努力學習";
+    let endStatement = "Thanks for your participation.";
+    let endStatement2 = "Keep on going!!";
     let scoreRatio = score / round;
 
     if(scoreRatio == 1){
-        endStatement = "好極！";
+        endStatement = "You are Excellent！";
     }
     else if(scoreRatio >= 0.8){
-        endStatement = "你表現得很好呀";
+        endStatement = "Well done!";
     }
     else if(scoreRatio >= 0.6){
-        endStatement = "你表現不錯呀";
+        endStatement = "Your performance is good.";
     }
     else if(scoreRatio >= 0.4){
-        endStatement = "非常欣賞你的努力";
+        endStatement = "Really appreciate your effort!";
     }
     
     if(!visible){
@@ -22,10 +22,11 @@ export default function ResultSummary({ score, round, visible }) {
     else{
 
         return (
-            <div className="flex flex-col items-center justify-between bg-yellow-100 border-indigo-900 border-4 rounded-md p-2">
-                <div className='text-6xl font-bold p-1 m-1'>你完成了 {round} 回合</div>
-                <div className='text-6xl font-bold p-1 m-1'>總分 {score} </div>
-                <div className='text-5xl font-bold p-1 m-1'> { endStatement + " " + endStatement2 } </div>
+            <div className="flex flex-col items-center justify-between bg-green-300/80 border-indigo-900 border-2 rounded-md p-2">
+                <div className='text-5xl font-bold p-1 m-1'>You have completed {round} rounds</div>
+                <div className='text-5xl font-bold p-1 m-1'>Total Score is {score} </div>
+                <div className='text-5xl text-blue-800 font-bold p-1 m-1'> { endStatement } </div>
+                <div className='text-5xl text-blue-800 font-bold p-1 m-1'> { endStatement2 } </div>
             </div>
         );
     }
