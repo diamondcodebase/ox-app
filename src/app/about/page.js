6a6c93280d8a73ff9bb20a212f6ce8e88dad15fa
diamond_dai <diamond.dai@gmail.com>
2024-11-11 23:20:27 -0700

    try to implement react scroll  motion in about page

--- src/app/about/page.js
+++ src/app/about/page.js
@@ -1,49 +1,88 @@
 "use client";
 import BasicButtonPanel from "@/components/BasicButtonPanel"
+import { Animator, ScrollContainer, ScrollPage, batch, Fade, FadeIn, FadeOut, Move, MoveIn, MoveOut, Sticky, StickyIn, StickyOut, Zoom, ZoomIn, ZoomOut } from "react-scroll-motion";
+// import { useSearchParams } from "next/navigation";
 // For passing current lang state
 //import Link from "next/link"; // import the Link Tag
 //import translation from "@/data/translation";
-import { useSearchParams } from "next/navigation";
+
 
 export default function About() {
-    const searchParams = useSearchParams();
+    const FadeMoveOut = batch(Sticky(), Fade(), MoveOut(0,-200));
+    const ZoomInScrollOut = batch(StickyIn(), FadeIn(), Zoom());
+    const FadeUp = batch(Fade(), Move(), Sticky());
+    const CastUp = batch(Sticky(), FadeIn(), MoveOut(-100,0));
+    const FadeSticky = batch(Fade(), Sticky());
 
     return (
-        <body>
-            <header></header>
-            <main className="flex min-h-screen flex-col items-center justify-between p-12 bg-clip-border"
-                style={{backgroundImage: `url(/about_BG.jpg)`,
-                    backgroundRepeat: "no-repeat",
-                    backgroundSize: "cover",
-                    backgroundAttachment: "fixed"                              
-            }}>
-                <div className='text-3xl font-bold p-3 m-3'>About This Web</div>
-                <div className="z-10 max-w-5xl w-full items-center justify-between font-mono text-sm lg:flex">
-                    <div className="row-auto text-xl bg-green-200/50 border-green-900 border-2 rounded-md p-3 text-gray-700 justify-center">
-                        The origin of this web came to me in 2021. During that year, we were still facing the threat of COVID-19, which led to bans on large gatherings. 
-                        This really affected our daily lives, including my church activities. At that time, my pastor was looking for interactive and creative ways to read the Bible with our church community. <br/>
-                        Inspired by an online game featuring true or false mini-games, I suggested to my pastor that I could create a Bible verse true or false game for our church group. 
-                        The early stages of development were quite challenging since I did not have any prior experience in web development.
-                        I learned about Google App Scripts through YouTube and tried to implement it using Google Drive and Google Sheets as a database. 
-                        Eventually, I was able to launch a series of Bible verse OX games, which my church group really enjoyed playing. <br/>
-                        This website is an extension of the original Bible OX game, and I have added two more features: a Hong Kong OX game and a Canada OX game. 
-                        I hope this will be helpful for you if you want to learn more about Hong Kong and Canada.
+        <ScrollContainer>
+            <div className='text-3xl font-bold p-3 m-3'>About This Web</div>
+            <ScrollPage>
+                <Animator animation={FadeMoveOut}>
+                    <h2>
+                        The origin of this web came to me in 2021.
+                    </h2>
+                    <h2>
+                        During that year, we were still facing the threat of COVID-19, which led to bans on large gatherings. 
+                    </h2>
+                </Animator>
+            </ScrollPage>
+            <ScrollPage>
+                <Animator animation={ZoomInScrollOut}>
+                    <h2>
+                        This really affected our daily lives, including my church activities.
+                    </h2>
+                </Animator>
+            </ScrollPage>
+            <ScrollPage>
+                <Animator animation={FadeUp}>
+                    <h2>
+                        At that time, my pastor was looking for interactive and creative ways to read the Bible with our church community.
+                    </h2>
+                </Animator>
+            </ScrollPage>
+            <ScrollPage>
+                <Animator animation={ZoomInScrollOut}>                
+                    
+                    <div className="z-10 max-w-5xl w-full items-center justify-between font-mono text-sm lg:flex">
+                        <div className="row-auto text-xl bg-green-200/50 border-green-900 border-2 rounded-md p-3 text-gray-700 justify-center">
+                            
+                              <br/>
+                            Inspired by an online game featuring true or false mini-games, I suggested to my pastor that I could create a Bible verse true or false game for our church group. 
+                            The early stages of development were quite challenging since I did not have any prior experience in web development.
+                            I learned about Google App Scripts through YouTube and tried to implement it using Google Drive and Google Sheets as a database. 
+                            Eventually, I was able to launch a series of Bible verse OX games, which my church group really enjoyed playing. <br/>
+                            This website is an extension of the original Bible OX game.
+                            I have added two more features: a Hong Kong OX game and a Canada OX game. 
+                            Hope this help you if you want to learn more about Hong Kong and Canada.
+                        </div>
                     </div>
-                </div>
-                <div className='text-3xl font-bold p-3 m-3'>About Me</div>
-                <div className="z-10 max-w-5xl w-full items-center justify-between font-mono text-sm lg:flex">
-                    <div className="row-auto text-xl bg-sky-200/50 border-sky-900 border-2 rounded-md p-3 text-gray-700 justify-center">
-                        My name is Diamond, and I've been in the tech industry for over 15 years. 
-                        I started my career in a scientific and engineering role after studying physics at university. 
-                        As time went on, a significant shift pushed me toward coding. 
-                        I was amazed by how software could automate tasks and unlock human potential. 
-                        In the last few years as a software developer, I love exploring new programming languages, frameworks, and technologies to enhance my skills. 
-                        My natural curiosity keeps me looking for the latest innovations and figuring out how they can solve real-world problems. 
-                        I also enjoy working with teams from different areas to bring projects to life.
+                </Animator>
+            </ScrollPage>
+            <ScrollPage>
+                <Animator animation={FadeSticky}>
+                    Enjoy!
+                </Animator>
+            </ScrollPage>
+            <ScrollPage>
+                <Animator animation={CastUp}>  
+                
+                    <div className='text-3xl font-bold p-3 m-3'>About Me</div>
+                    <div className="z-10 max-w-5xl w-full items-center justify-between font-mono text-sm lg:flex">
+                        <div className="row-auto text-xl bg-sky-200/50 border-sky-900 border-2 rounded-md p-3 text-gray-700 justify-center">
+                            My name is Diamond, and I've been in the tech industry for over 15 years. 
+                            I started my career in a scientific and engineering role after studying physics at university. 
+                            As time went on, a significant shift pushed me toward coding. 
+                            I was amazed by how software could automate tasks and unlock human potential. 
+                            In the last few years as a software developer, I love exploring new programming languages, frameworks, and technologies to enhance my skills. 
+                            My natural curiosity keeps me looking for the latest innovations and figuring out how they can solve real-world problems. 
+                            I also enjoy working with teams from different areas to bring projects to life.
+                        </div>
                     </div>
-                </div>
-                <BasicButtonPanel />
-            </main>
-        </body>
+                </Animator>
+            </ScrollPage>
+            <BasicButtonPanel />
+
+        </ScrollContainer>
     );
 }
