4072b9560b392a88a0410d3e9314482a0817d3e8
diamond_dai <diamond.dai@gmail.com>
2024-11-13 22:58:49 -0700

    install framer-motion and implement horizontal scroll in about me section

--- src/app/about/page.js
+++ src/app/about/page.js
@@ -1,6 +1,9 @@
 "use client";
 import BasicButtonPanel from "@/components/BasicButtonPanel"
 import { Animator, ScrollContainer, ScrollPage, batch, Fade, FadeIn, FadeOut, Move, MoveIn, MoveOut, Sticky, StickyIn, StickyOut, Zoom, ZoomIn, ZoomOut } from "react-scroll-motion";
+import { motion, useTransform, useScroll } from "framer-motion";
+import { useRef } from "react";
+
 // import { useSearchParams } from "next/navigation";
 // For passing current lang state
 //import Link from "next/link"; // import the Link Tag
@@ -12,89 +15,188 @@ export default function About() {
     const ZoomInScrollOut = batch(ZoomIn(), FadeIn(), Sticky(), MoveOut(0,-200));
     const FadeUp = batch(Fade(), Move(), Sticky());
     const CastUp = batch(Sticky(), FadeIn(), MoveOut(-100,0));
-    const FadeSticky = batch(Fade(), Sticky(), FadeOut());
+    const FadeSticky = batch(Fade(), Sticky(), FadeOut(), MoveOut(-200,0));
 
     return (
-        <ScrollContainer>
-            <div className='text-3xl font-bold p-3 m-3'>About This Web</div>
-            <ScrollPage>
-                <Animator animation={FadeMoveOut}>
-                    <div className='text-2xl font-bold p-3 m-3'>
-                        The origin of this web came to me in 2021.
-                    </div>
-                    <div className='text-2xl font-bold p-3 m-3'>
-                        During that year, we were still facing the threat of COVID-19, which led to bans on large gatherings. 
-                    </div>
-                    <div className='text-2xl font-bold p-3 m-3'>
-                        This really affected our daily lives, including my church activities.
-                    </div>
-                </Animator>
-            </ScrollPage>
-            <ScrollPage>
-                <Animator animation={FadeUp}>
-                    <div className='text-2xl font-bold p-3 m-3'>
-                        At that time, my pastor was looking for interactive and creative ways to read the Bible with our church community.
-                    </div>
-                </Animator>
-            </ScrollPage>
-            <ScrollPage>
-                <Animator animation={ZoomInScrollOut}>
-                    <div className='text-2xl font-bold p-3 m-3'>
-                        Inspired by an online game featuring true or false mini-games, I suggested to my pastor that I could create a Bible verse true or false game for our church group.
-                    </div>
-                </Animator>
-            </ScrollPage>
-            <ScrollPage>
-                <Animator animation={FadeUp}>
-                    <div className='text-2xl font-bold p-3 m-3'>
-                        The early stages of development were quite challenging since I did not have any prior experience in web development.
-                    </div>
-                    <div className='text-2xl font-bold p-3 m-3'>
-                        I learned about Google App Scripts through YouTube and tried to implement it using Google Drive and Google Sheets as a database.
-                    </div>
-                </Animator>
-            </ScrollPage>
-            <ScrollPage>
-                <Animator animation={ZoomInScrollOut}>
-                    <div className='text-2xl font-bold p-3 m-3'>
-                        Eventually, I was able to launch a series of Bible verse OX games, which my church group really enjoyed playing.
-                    </div>
-                </Animator>
-            </ScrollPage>
-            <ScrollPage>
-                <Animator animation={FadeUp}>             
-                    <div className='text-2xl font-bold p-3 m-3'>
-                        This website is an extension of the original Bible OX game.
-                        I have added two more features: a Hong Kong OX game and a Canada OX game. 
-                    </div>
-                </Animator>
-            </ScrollPage>
-            <ScrollPage>
-                <Animator animation={FadeSticky}>
-                    <div className='text-2xl font-bold p-3 m-3'>
-                        Hope you Enjoy!
-                    </div>                
-                </Animator>
-            </ScrollPage>
-            <ScrollPage>
-                <Animator animation={CastUp}>  
-                
-                    <div className='text-3xl font-bold p-3 m-3'>About Me</div>
-                    <div className="z-10 max-w-5xl w-full items-center justify-between font-mono text-sm lg:flex">
-                        <div className="row-auto text-xl bg-sky-200/50 border-sky-900 border-2 rounded-md p-3 text-gray-700 justify-center">
-                            My name is Diamond, and I've been in the tech industry for over 15 years. 
-                            I started my career in a scientific and engineering role after studying physics at university. 
-                            As time went on, a significant shift pushed me toward coding. 
-                            I was amazed by how software could automate tasks and unlock human potential. 
-                            In the last few years as a software developer, I love exploring new programming languages, frameworks, and technologies to enhance my skills. 
-                            My natural curiosity keeps me looking for the latest innovations and figuring out how they can solve real-world problems. 
-                            I also enjoy working with teams from different areas to bring projects to life.
+        <div>
+            <ScrollContainer>
+                <div className='text-3xl font-bold p-3 m-3'>About This Web</div>
+                <ScrollPage>
+                    <Animator animation={FadeMoveOut}>
+                        <div className='text-2xl font-bold p-3 m-3'>
+                            The origin of this web came to me in 2021.
                         </div>
-                    </div>
-                </Animator>
-            </ScrollPage>
+                        <div className='text-2xl font-bold p-3 m-3'>
+                            During that year, we were still facing the threat of COVID-19, which led to bans on large gatherings. 
+                        </div>
+                        <div className='text-2xl font-bold p-3 m-3'>
+                            This really affected our daily lives, including my church activities.
+                        </div>
+                    </Animator>
+                </ScrollPage>
+                <ScrollPage>
+                    <Animator animation={FadeUp}>
+                        <div className='text-2xl font-bold p-3 m-3'>
+                            At that time, my pastor was looking for interactive and creative ways to read the Bible with our church community.
+                        </div>
+                    </Animator>
+                </ScrollPage>
+                <ScrollPage>
+                    <Animator animation={ZoomInScrollOut}>
+                        <div className='text-2xl font-bold p-3 m-3'>
+                            Inspired by an online game featuring true or false mini-games, I suggested to my pastor that I could create a Bible verse true or false game for our church group.
+                        </div>
+                    </Animator>
+                </ScrollPage>
+                <ScrollPage>
+                    <Animator animation={FadeUp}>
+                        <div className='text-2xl font-bold p-3 m-3'>
+                            The early stages of development were quite challenging since I did not have any prior experience in web development.
+                        </div>
+                        <div className='text-2xl font-bold p-3 m-3'>
+                            I learned about Google App Scripts through YouTube and tried to implement it using Google Drive and Google Sheets as a database.
+                        </div>
+                    </Animator>
+                </ScrollPage>
+                <ScrollPage>
+                    <Animator animation={ZoomInScrollOut}>
+                        <div className='text-2xl font-bold p-3 m-3'>
+                            Eventually, I was able to launch a series of Bible verse OX games, which my church group really enjoyed playing.
+                        </div>
+                    </Animator>
+                </ScrollPage>
+                <ScrollPage>
+                    <Animator animation={FadeUp}>             
+                        <div className='text-2xl font-bold p-3 m-3'>
+                            This website is an extension of the original Bible OX game.
+                            I have added two more features: a Hong Kong OX game and a Canada OX game. 
+                        </div>
+                    </Animator>
+                </ScrollPage>
+                <ScrollPage>
+                    <Animator animation={FadeSticky}>
+                        <div className='text-2xl font-bold p-3 m-3'>
+                            Hope you Enjoy!
+                        </div>                
+                    </Animator>
+                </ScrollPage>
+            </ScrollContainer>
+            <div className="bg-neutral-800">
+                <div className="flex h-48 items-center justify-center">
+                    <span className="font-semibold uppercase text-neutral-500">
+                        Scroll down
+                    </span>
+                </div>
+                <HorizontalScrollCarousel />
+                <div className="flex h-48 items-center justify-center">
+                    <span className="font-semibold uppercase text-neutral-500">
+                        Scroll up
+                    </span>
+                </div>
+            </div>
+           
             <BasicButtonPanel />
-
-        </ScrollContainer>
+        </div>        
     );
 }
+
+const HorizontalScrollCarousel = () => {
+  const targetRef = useRef(null);
+  const { scrollYProgress } = useScroll({
+    target: targetRef,
+  });
+
+  const x = useTransform(scrollYProgress, [0, 1], ["1%", "-95%"]);
+
+  return (
+    <section ref={targetRef} className="relative h-[300vh] bg-neutral-900">
+      <div className="sticky top-0 flex h-screen items-center overflow-hidden">
+        <motion.div style={{ x }} className="flex gap-4">
+          {cards.map((card) => {
+            return <Card card={card} key={card.id} />;
+          })}
+        </motion.div>
+      </div>
+    </section>
+  );
+};
+
+const Card = ({ card }) => {
+  return (
+    <div
+      key={card.id}
+      className="group relative h-[300px] w-[400px] overflow-hidden bg-neutral-200"
+    >
+      <div
+        style={{
+          backgroundImage: `url(${card.url})`,
+          backgroundSize: "cover",
+          backgroundPosition: "center",
+        }}
+        className="absolute inset-0 z-0 transition-transform duration-300 group-hover:scale-110"
+      ></div>
+      <div className="absolute inset-0 z-10 grid">
+        <p className="bg-gradient-to-br from-white/20 to-white/0 p-5 text-xl font-black text-white backdrop-blur-lg">
+          {card.title}
+        </p>
+        <p className="bg-gradient-to-br from-white/20 to-white/0 p-5 text-xl font-black text-white backdrop-blur-lg">
+          {card.title2}
+        </p>
+      </div>
+    </div>
+  );
+};
+
+const cards = [
+    {
+        url: "/imgs/abstract/1.jpg",
+        title: "Nice to meet you!",
+        title2: "My name is Diamond.",
+        id: 1,
+    },
+    {
+        url: "/imgs/abstract/1.jpg",
+        title: "I've been in the tech industry for over 15 years.",
+        id: 2,
+    },
+    {
+        url: "/imgs/abstract/2.jpg",
+        title: "I started my career in a scientific and engineering role.",
+        id: 3,
+    },
+    {
+      url: "/imgs/abstract/3.jpg",
+      title: "As time went on, a significant shift pushed me toward coding.",
+      id: 3,
+    },
+    {
+      url: "/imgs/abstract/4.jpg",
+      title: "I was amazed by how software could automate tasks.",
+      id: 4,
+    },
+    {
+      url: "/imgs/abstract/5.jpg",
+      title: "Being a software developer, I am happy to explore this new tech field.",
+      id: 5,
+    },
+    {
+        url: "/imgs/abstract/5.jpg",
+        title: "It gives me chances to learn new programming languages, frameworks, and technologies",
+        id: 5,
+    },
+    {
+        url: "/imgs/abstract/6.jpg",
+        title: "My curiosity keeps me seeking how the latest innovations can solve real-world problems.",
+        id: 6,
+    },
+    {
+        url: "/imgs/abstract/7.jpg",
+        title: "Working with teams with different talents, bringing projects to life drive my motivation.",
+        id: 7,
+    },
+    {
+        url: "/imgs/abstract/8.jpg",
+        title: "I would be excited if one day I could work with you, fork~",
+        id: 8,
+    },
+  ];
