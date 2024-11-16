2559e9a2d469aa10b9f173c4b9fc012897a3ad38
diamond_dai <diamond.dai@gmail.com>
2024-11-15 22:44:36 -0700

    implement motion button in CustomerButton component

--- src/components/CustomButton.js
+++ src/components/CustomButton.js
@@ -1,16 +1,18 @@
+import { motion } from 'framer-motion';
+
 export default function CustomButton({btnLabel, onClickFn, hrefLink}){
     if(hrefLink !== null && hrefLink != ""){
         return (
-            <button className="text-3xl bg-gray-500 border-gray-300 border-4 rounded-md p-4 m-4">
+            <motion.button whileTap={{ scale: 0.85 }} className="text-3xl bg-gray-500 border-gray-300 border-4 rounded-md p-4 m-4">
                 <a href={hrefLink}>
                     <span className="text-gray-200">{btnLabel}</span>
                 </a>
-            </button>
+            </motion.button>
         )
     }
     return (
-        <button className="text-3xl bg-gray-500 border-gray-300 border-4 rounded-md p-4 m-4" onClick={onClickFn}>
+        <motion.button whileTap={{ scale: 0.85 }} className="text-3xl bg-gray-500 border-gray-300 border-4 rounded-md p-4 m-4" onClick={onClickFn}>
             <span className="text-gray-200">{btnLabel}</span>
-        </button>
+        </motion.button>
     );
 }
