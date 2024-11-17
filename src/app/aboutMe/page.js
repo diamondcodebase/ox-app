66a1a94fd42bb3d81e872387a1b036b8b2c9c48e
diamond_dai <diamond.dai@gmail.com>
2024-11-16 23:27:24 -0700

    modify aboutme page and implement component testing

--- src/app/aboutMe/page.js
+++ src/app/aboutMe/page.js
@@ -1,13 +1,25 @@
 "use client";
 import TextTyping from "@/components/TextTyping";
+import Stagger from "@/components/Stagger";
 import BasicButtonPanel from "@/components/BasicButtonPanel";
 
 export default function AboutMe() {
-    const text1 = "Nice to meet you! My name is Diamond.";
+    const sentence1 = "Nice to meet you! My name is Diamond.";
+    const sentence2 = "I've been in the tech industry for over 15 years.";
+    const sentence3 = "I started my career in a scientific and engineering role.";
+    const sentence4 = "As time went on, a significant shift pushed me toward coding.";
+    const sentence5 = "I was amazed by how software could automate tasks.";
+    const sentence6 = "Being a software developer, I am happy to explore this new tech field.";
+    const sentence7 = "My curiosity keeps me seeking how the latest innovations can solve real-world problems.";
+    const sentence8 = "Working with teams with different talents, bringing projects to life drive my motivation.";
+    const sentence9 = "I would be excited if one day I could work with you, fork~";
 
     return (
-        <div>
-            <TextTyping textInput={text1} />
-        </div>
+        <main>
+            
+            <TextTyping textInput={sentence1} />
+            <Stagger />
+
+        </main>
     );
 }
