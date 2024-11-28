a01314512876a4975b80091536c09b9222c8aa09
diamond_dai <diamond.dai@gmail.com>
2024-11-27 23:29:35 -0700

    Optimize Section5 component

--- src/components/Section5.js
+++ src/components/Section5.js
@@ -2,14 +2,6 @@ import React from 'react';
 import { motion } from "framer-motion";
 
 export default function Section5({displayText, imageSrc, alt}) {
-    // const displayCoding = "Sub AutoFitRows()";
-    // const displayCoding2 = "Cells.Select";
-    // const displayCoding3 = "Cells.EntireRow.AutoFit";
-    // const displayCoding4 = "End Sub";
-    // const displayCodingSplit = displayCoding.split(" ");
-    // const displayCodingSplit2 = displayCoding2.split(" ");
-    // const displayCodingSplit3 = displayCoding3.split(" ");
-    // const displayCodingSplit4 = displayCoding4.split(" ");
     return (
         <div>
             {
@@ -18,70 +10,6 @@ export default function Section5({displayText, imageSrc, alt}) {
                         { displayText }
                     </div>
                     <div>
-                        {/* <div className='text-lg font-bold p-3 m-3 items-start'>
-                            <section id="codingDisplay">
-                                <div>
-                                    {displayCodingSplit.map((el, i) => (
-                                        <motion.span
-                                        initial={{ opacity: 0 }}
-                                        animate={{ opacity: 1 }}
-                                        transition={{
-                                            duration: 0.5,
-                                            delay: i / 5,
-                                        }}
-                                        key={i}
-                                        >
-                                        {el}{" "}
-                                        </motion.span>
-                                    ))}
-                                </div>
-                                <div>
-                                    {displayCodingSplit2.map((el, i) => (
-                                        <motion.span
-                                        initial={{ opacity: 0 }}
-                                        animate={{ opacity: 1 }}
-                                        transition={{
-                                            duration: 0.5,
-                                            delay: i / 5 + 1,
-                                        }}
-                                        key={i}
-                                        >
-                                        {el}{" "}
-                                        </motion.span>
-                                    ))}
-                                </div>
-                                <div>
-                                    {displayCodingSplit3.map((el, i) => (
-                                        <motion.span
-                                        initial={{ opacity: 0 }}
-                                        animate={{ opacity: 1 }}
-                                        transition={{
-                                            duration: 0.5,
-                                            delay: i / 5 + 2,
-                                        }}
-                                        key={i}
-                                        >
-                                        {el}{" "}
-                                        </motion.span>
-                                    ))}
-                                </div>
-                                <div>
-                                    {displayCodingSplit4.map((el, i) => (
-                                        <motion.span
-                                        initial={{ opacity: 0 }}
-                                        animate={{ opacity: 1 }}
-                                        transition={{
-                                            duration: 0.5,
-                                            delay: i / 5 + 3,
-                                        }}
-                                        key={i}
-                                        >
-                                        {el}{" "}
-                                        </motion.span>
-                                    ))}
-                                </div>
-                            </section>
-                        </div> */}
                         <img src= { imageSrc } alt= { alt } />
                     </div>
                     
