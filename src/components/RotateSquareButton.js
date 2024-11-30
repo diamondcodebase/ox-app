3eccfc88b673b20ce54ddecd4211ecde0892eb6f
diamond_dai <diamond.dai@gmail.com>
2024-11-29 23:37:56 -0700

    RotateSquareButton update

--- src/components/RotateSquareButton.js
+++ src/components/RotateSquareButton.js
@@ -2,24 +2,52 @@ import { motion, useCycle } from "framer-motion";
 import { useState } from "react";
 
 export default function RotateSquareButton() {
-    const [animate, cycle] = useCycle(
-        { scale: 1, rotate: 0 },
-        { scale: 1.2, rotate: 90 }
-    );
+    const [isRotated, setIsRotated] = useState(false);
+
+    const handleClick = () => {
+        setIsRotated((prevState) => !prevState);
+    };
+
+    const softwareDeveloper = "Software Developer";
 
     return (
-        <motion.div
+        <motion.div 
+            className="flex flex-col items-center justify-center"
             style={{
                 width: 200,
                 height: 200,
                 borderRadius: 40,
-                backgroundColor: "#ccc",
+                padding: 10,
+                margin: 10,
                 cursor: "pointer"
             }}
-            animate={animate}
-            onTap={cycle}
+            animate={{
+                rotate: isRotated ? 0 : 90,
+                scale: isRotated ? 1.5 : 1,
+                backgroundColor: isRotated? "#dd4" : "#8dd",
+                padding: isRotated ? 10 : 10,
+                margin: isRotated ? 10 : 10,
+            }}
+            transition={{
+                duration: 0.5,
+                ease: 'easeInOut'
+            }}
+            onClick={handleClick}
         >
-            <p>Engineer</p>
+            <motion.div
+                className="font-bold"
+                animate={{
+                    rotate: isRotated? 0 : -90,
+                    padding: isRotated? 10 : 10,
+                    scale: isRotated ? 1.25 : 1.5,
+                }}
+                transition={{
+                    duration: 0.5,
+                    ease: 'easeInOut'
+                }}
+            >
+                {isRotated ? "Software Developer" : "Engineer" }
+            </motion.div>
         </motion.div>
     );
 }
