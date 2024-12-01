4033d4e6946f8f6d28b2f405f44fac6d21b4982b
diamond_dai <diamond.dai@gmail.com>
2024-11-30 23:43:59 -0700

    modify RotateSquareButton component and section4

--- src/components/RotateSquareButton.js
+++ src/components/RotateSquareButton.js
@@ -1,7 +1,7 @@
-import { motion, useCycle } from "framer-motion";
+import { motion, AnimatePresence } from "framer-motion";
 import { useState } from "react";
 
-export default function RotateSquareButton() {
+export default function RotateSquareButton({displayText}) {
     const [isRotated, setIsRotated] = useState(false);
 
     const handleClick = () => {
@@ -11,43 +11,66 @@ export default function RotateSquareButton() {
     const softwareDeveloper = "Software Developer";
 
     return (
-        <motion.div 
-            className="flex flex-col items-center justify-center"
+        <motion.div className="flex flex-col items-center justify-between"
+            initial={{ opacity: 0 }}
+            animate={{ opacity: 1 }}
+            exit={{ opacity: 0 }}
+            transition={{ duration: 0.5 }}
             style={{
-                width: 200,
-                height: 200,
-                borderRadius: 40,
-                padding: 10,
-                margin: 10,
-                cursor: "pointer"
+                height: '100vh',
+                display: 'flex',
+                justifyContent: 'center',
+                alignItems: 'center',
+                backgroundImage: isRotated?
+                'url("./software_develop.png")'
+                : 'url("./engineer_lab.png")',
+                backgroundSize: 'cover',
+                backgroundPosition: 'center',
             }}
-            animate={{
-                rotate: isRotated ? 0 : 90,
-                scale: isRotated ? 1.5 : 1,
-                backgroundColor: isRotated? "#dd4" : "#8dd",
-                padding: isRotated ? 10 : 10,
-                margin: isRotated ? 10 : 10,
-            }}
-            transition={{
-                duration: 0.5,
-                ease: 'easeInOut'
-            }}
-            onClick={handleClick}
         >
-            <motion.div
-                className="font-bold"
+            <div className='text-xl sm:text-3xl font-bold p-3 m-3'>
+                { displayText }
+            </div>
+            <motion.div 
+                className="flex flex-col items-center justify-center"
+                style={{
+                    width: 200,
+                    height: 200,
+                    borderRadius: 40,
+                    padding: 10,
+                    margin: 10,
+                    cursor: "pointer"
+                }}
                 animate={{
-                    rotate: isRotated? 0 : -90,
-                    padding: isRotated? 10 : 10,
-                    scale: isRotated ? 1.25 : 1.5,
+                    rotate: isRotated ? 0 : 90,
+                    scale: isRotated ? 1.5 : 1,
+                    backgroundColor: isRotated? "#dd4" : "#8dd",
+                    padding: isRotated ? 10 : 10,
+                    margin: isRotated ? 80 : 80,
                 }}
                 transition={{
                     duration: 0.5,
                     ease: 'easeInOut'
                 }}
+                onClick={handleClick}
             >
-                {isRotated ? "Software Developer" : "Engineer" }
+                <motion.div
+                    className="font-bold"
+                    animate={{
+                        rotate: isRotated? 0 : -90,
+                        padding: isRotated? 10 : 10,
+                        scale: isRotated ? 1.25 : 1.5,
+                    }}
+                    transition={{
+                        duration: 0.5,
+                        ease: 'easeInOut'
+                    }}
+                >
+                    {isRotated ? "Software Developer" : "Engineer" }
+                </motion.div>
             </motion.div>
+
         </motion.div>
+
     );
 }
