5f78bb3cf297e9e109b050fc7bb458001e5236c9
diamond_dai <diamond.dai@gmail.com>
2024-12-06 16:41:05 -0700

    introduce a method to display card at the bottom

--- src/components/FlashCardStack.js
+++ src/components/FlashCardStack.js
@@ -8,8 +8,8 @@ import {
 
 function Card(props) {
     const [exitX, setExitX] = useState(0);
-    const techArr = ["VB.Net", "C#", "MSSQL"];
-
+    const techArr = ["Java","VB.Net","C#","MSSQL","Restful API","JavaScript","PHP","AWS","MySQL","VueJS","Python","Machine Learning"];
+    
     const x = useMotionValue(0);
     const scale = useTransform(x, [-150, 0, 150], [0.5, 1, 0.5]);
     const rotate = useTransform(x, [-150, 0, 150], [-45, 0, 45], {
@@ -38,11 +38,13 @@ function Card(props) {
         if (info.offset.x < -100) {
             setExitX(-250);
             props.setIndex(props.index + 1);
+            props.setRound(props.round+1);
         }
         if (info.offset.x > 100) {
             setExitX(250);
             props.setIndex(props.index + 1);
-        }
+            props.setRound(props.round+1);
+        }        
     }
 
     return (
@@ -78,6 +80,7 @@ function Card(props) {
                 style={{
                     width: 200,
                     height: 200,
+                    color:"#000",
                     backgroundColor: "#EEDD44",
                     borderRadius: 30,
                     scale,
@@ -86,12 +89,15 @@ function Card(props) {
             >
                 <div>{assignCardText()}</div>
             </motion.div>
+            
         </motion.div>
     );
 }
 
 export default function FlashCardStack({displayText}) {
     const [index, setIndex] = useState(0);
+    const [round, setRound] = useState(0);
+    const techArr = ["Java","VB.Net","C#","MSSQL","Restful API","JavaScript","PHP","AWS","MySQL","VueJS","Python","Machine Learning"];
 
     return (
         <motion.div className="flex flex-col items-center justify-between"
@@ -100,9 +106,9 @@ export default function FlashCardStack({displayText}) {
                 display: 'flex',
                 justifyContent: 'center',
                 alignItems: 'center',
+                color: '#fff',
                 backgroundColor: '#554422',
                 backgroundSize: 'cover',
-                backgroundPosition: 'center',
             }}
         >
             <div className='text-xl sm:text-3xl font-bold p-3 m-3'>
@@ -115,11 +121,107 @@ export default function FlashCardStack({displayText}) {
                         key={index}
                         frontCard={true}
                         index={index}
+                        round={round}
+                        techArr={techArr}
                         setIndex={setIndex}
+                        setRound={setRound}
                         drag="x"
                     />
                 </AnimatePresence>
+                <AnimatePresence>
+                    {round > 0 ? (
+                        <motion.div
+                            className="flex flex-col items-center justify-between text-xl sm:text-2xl font-bold p-3 m-3"
+                            style={{ 
+                                x:-50,
+                                y:250, 
+                                position: "relative", 
+                                backgroundColor: '#777700',
+                                borderRadius: 10,
+                                justifyContent: 'center',
+                            }}
+                            initial={{ opacity: 0, scale: 0.8 }}
+                            animate={{ opacity: 1, scale: 1.2 }}
+                        >
+                            {techArr[0]}
+                        </motion.div>
+                    ) : null}
+                    {round > 1 ? (
+                        <motion.div
+                            className="flex flex-col items-center justify-between text-xl sm:text-2xl font-bold p-3 m-3"
+                            style={{ 
+                                x:-30,
+                                y:300, 
+                                position: "relative", 
+                                backgroundColor: '#777700',
+                                borderRadius: 10,
+                                justifyContent: 'center',
+                            }}
+                            initial={{ opacity: 0, scale: 0.8 }}
+                            animate={{ opacity: 1, scale: 1.2 }}
+                        >
+                            {techArr[1]}
+                        </motion.div>
+                    ) : null}
+                    {round > 2 ? (
+                        <motion.div
+                            className="flex flex-col items-center justify-between text-xl sm:text-2xl font-bold p-3 m-3"
+                            style={{ 
+                                heigth:80, 
+                                width:80, 
+                                x:40,
+                                y:250, 
+                                position: "relative", 
+                                backgroundColor: '#777700',
+                                borderRadius: 10,
+                                justifyContent: 'center',
+                            }}
+                            initial={{ opacity: 0, scale: 0.8 }}
+                            animate={{ opacity: 1, scale: 1.2 }}
+                        >
+                            {techArr[2]}
+                        </motion.div>
+                    ) : null}
+                    {round > 3 ? (
+                        <motion.div
+                            className="flex flex-col items-center justify-between text-xl sm:text-2xl font-bold p-3 m-3"
+                            style={{ 
+                                heigth:80, 
+                                width:80, 
+                                x:-50,
+                                y:250, 
+                                position: "relative", 
+                                backgroundColor: '#777700',
+                                borderRadius: 10,
+                                justifyContent: 'center',
+                            }}
+                            initial={{ opacity: 0, scale: 0.8 }}
+                            animate={{ opacity: 1, scale: 1.2 }}
+                        >
+                            {techArr[3]}
+                        </motion.div>
+                    ) : null}
+                    {round > 4 ? (
+                        <motion.div
+                            className="flex flex-col items-center justify-between text-xl sm:text-2xl font-bold p-3 m-3"
+                            style={{ 
+                                heigth:80, 
+                                width:80, 
+                                x:-50,
+                                y:250, 
+                                position: "relative", 
+                                backgroundColor: '#777700',
+                                borderRadius: 10,
+                                justifyContent: 'center',
+                            }}
+                            initial={{ opacity: 0, scale: 0.8 }}
+                            animate={{ opacity: 1, scale: 1.2 }}
+                        >
+                            {techArr[4]}
+                        </motion.div>
+                    ) : null}
+                </AnimatePresence>
             </motion.div>
-        </motion.div>        
+        </motion.div>       
     );
 }
