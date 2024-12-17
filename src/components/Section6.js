2251015883dfdb1800d262267840d14b5729b10e
diamond_dai <diamond.dai@gmail.com>
2024-12-16 22:55:27 -0700

    create scrollRevealCard and modify section 6

--- src/components/Section6.js
+++ src/components/Section6.js
@@ -1,12 +1,18 @@
 import FlashCardStack from "@/components/FlashCardStack";
+import ScrollRevealCard from "./ScrollRevealCard";
 
 export default function Section7({displayText}) {
+    const items = ["Real-time Parking Lot Vacancy Report", "E-Form", "Auto Payment Kiosk", "Reservation APIs", "Synchronization System"];
     return (
-        <div className='flex flex-col items-center justify-between'>
+        <div id="section" className=''>
             <div className='text-2xl sm:text-4xl font-bold p-3 m-3'>
                 { displayText }
             </div>
-           
+            <div className="ScrollCards">
+                {items.map((item, i) => (
+                    <ScrollRevealCard key={i} text={item} index={i} />
+                ))}
+            </div>           
         </div>
     );
 };
